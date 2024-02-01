3d6a764943e0612e3aa70d2e8b2c0d87e98107a7
Bashu-011 <bashu2609@gmail.com>
2024-02-01 03:15:58 +0300

    Practice lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,13 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+    function focus(){
+        console.log("Good!")
+    }
+    return(
+        <div>
+            <button onFocus={focus} onBlur={() => console.log('Hey! Eyes on me!')}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe
